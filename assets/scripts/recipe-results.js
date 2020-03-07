810fdced9d84226e944db6e55be3306ea459ef18
Yfopla <ekblecker@gmail.com>
2020-03-07 13:26:25 -0500

    updated

--- assets/scripts/recipe-results.js
+++ assets/scripts/recipe-results.js
@@ -6,5 +6,24 @@ $(document).ready(function() {
 
     $(".results-subtitle").append(localStorage.getItem("search"))
 
+    // Displays array into the results page
+    function showNames() {
+        for(var i = 0; i < recipes.length; i++){
+            
+            let div = $("<div>")
+            let resultItem = $(".recipe-result-content")
+            let resultName = $(".is-6")
+            
+    
+            $(resultName).append('<div>' + recipes[i].title + '</div>')
+            //   resultName.append(recipes[i].title)
+             
+    
+            // $(recipe[i]).appendto(resultItem.resultName)
+        
+            // resultItem.resultName.append(recipes.title)
+        }
+        }
+        showNames()
 });
 
