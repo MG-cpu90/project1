f6027c3738e4ad45e5cf599b346f93ead2fd6707
Yfopla <ekblecker@gmail.com>
2020-03-10 15:12:11 -0400

    Added: clickable link to recipe page

--- assets/scripts/recipe-results.js
+++ assets/scripts/recipe-results.js
@@ -5,6 +5,7 @@ $(document).ready(function() {
     console.log(recipes);
 
     $(".results-subtitle").append(localStorage.getItem("search"))
+    // var queryURL2 = "https://api.spoonacular.com/recipes/" + recipe.id + "/summary?apiKey=18f2f6ffa1da41b0b161e90498f0d67a"
 
 
     printResults(recipes)
@@ -21,13 +22,16 @@ function printResults(recipes){
 
 function printResult(recipe){
 
+  
+
     var cardEl = $("<div>").addClass("card recipe-result").attr("data-id",recipe.id);
     var cardContentEl = $("<div>").addClass("card-content recipe-result-content");
     var mediaEl = $("<div>").addClass("media");
     var mediaLeftEl = $("<div>").addClass("media-left");
     var figureEl = $("<figure>").addClass("image is-96x96");
     var imgEl = $("<img>").addClass("result-thumbnail").attr("src","https://spoonacular.com/recipeImages/" + recipe.image).attr("alt",recipe.title);
-    
+    // var SumEl = $("<div>").addClass("content is-small").attr("https://api.spoonacular.com/recipes/" + recipe.id + "/summary?apiKey=18f2f6ffa1da41b0b161e90498f0d67a")
+
     figureEl.append(imgEl);
     mediaLeftEl.append(figureEl);
 
@@ -35,11 +39,12 @@ function printResult(recipe){
     var recipeNameEl = $("<p>").addClass("is-6 recipe-name").text(recipe.title)
     
     var popoverEl = getPopOver();
+    // var sumCont = sumEL();
     
     var contentEl = $("<div>").addClass("content is-small").text("Ready In: " + recipe.readyInMinutes + " minutes")
     var contentEl2 = $("<div>").addClass("content is-small").text("Serves up to: " + recipe.servings + " people");
 
-    mediaContentEl.append(recipeNameEl).append(popoverEl).append(contentEl).append(contentEl2);
+    mediaContentEl.append(recipeNameEl).append(popoverEl).append(contentEl).append(contentEl2)
 
     mediaEl.append(mediaLeftEl).append(mediaContentEl);
 
@@ -48,6 +53,11 @@ function printResult(recipe){
     cardEl.append(cardContentEl);
 
     $(".results").append(cardEl);
+
+    $(".recipe-result-content").on("click", function() {
+        window.location.href = "recipe.html";
+    
+    })
 
 }
 
@@ -64,6 +74,20 @@ function getPopOver(){
 
     return popoverEl;
 }
+
+
+// $(".recipe-result-content").on("click", function() {
+//     console.log("hello")
+//     window.location.href = "recipe.html";
+
+// })
+
+// function SumEl (){
+
+//     var sumCon = "https://api.spoonacular.com/recipes/" + recipe.id + "/summary?apiKey=18f2f6ffa1da41b0b161e90498f0d67a"
+
+//     return sumCon;
+// }
 
 
 
